3e7379eeb4ac0694eaf2aeda31cfd1625086edae
lipemat <mat@matlipe.com>
2021-01-07 09:30:59 -0700

    Switch main file to `index.js` to make easier to see

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+	'extends': '@wordpress/stylelint-config',
+	'rules': {
+		'function-parentheses-space-inside': null,
+		'indentation': 'tab',
+		'no-invalid-double-slash-comments': null,
+		'number-leading-zero': null,
+		'property-no-unknown': [
+			true,
+			{
+				'ignoreProperties': [
+					'/\/\//',
+					'/^composes/',
+				],
+			},
+		],
+		'rule-empty-line-before': [
+			'always',
+			{
+				'ignore': [
+					'after-comment',
+					'first-nested',
+				],
+			},
+		],
+		'selector-class-pattern': null,
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				'ignorePseudoClasses': [
+					'global',
+				],
+			},
+		],
+		'string-quotes': null,
+	},
+};
